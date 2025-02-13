38b4d3e491d0daa9ceacd0a9c46aca4e39589008
Sameer Mallik <sameer2.2mallik@gmail.com>
2025-02-13 00:58:20 -0800

    Update main.py
    fixed code

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-def min_operations_to_seven(n):
-    if '7' in str(n):
-        return 0
-    add = 9
-    ops = 1
-    while '7' not in str(n + add):
-        if add > 10**9:  
-            break
-        add = add * 10 + 9  
-        ops += 1
-    return ops
+def min_operations_to_make_seven(t, test_cases):
+    results = []
+    
+    for n in test_cases:
+        count = 0
+        while '7' not in str(n):  
+            count += 1
+            n += int('9' * count)  
+        results.append(count)
+    
+    return results
+
+#preset test cases cuz replit devs are greedy and i couldn't test input
+test_cases = [80, 999, 555, 777]
+t = len(test_cases)
 
 
-def process_test_cases():
-    t = int(input())  
-    test_cases = [int(input()) for _ in range(t)]
-    results = [str(min_operations_to_seven(n)) for n in test_cases]
-    print("\n".join(results))
-
-
-process_test_cases()
+results = min_operations_to_make_seven(t, test_cases)
+for result in results:
+    print(result)
